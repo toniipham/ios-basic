6e3beb3cb050f65f527a6fd209b8167075f0dda1
toniipham <macbookpro@Macbooks-MacBook-Air.local>
2016-10-22 09:25:17 +0700

    check valid type and date

--- basicios-161020-01/basicios-161020-01/ViewController.swift
+++ basicios-161020-01/basicios-161020-01/ViewController.swift
@@ -27,22 +27,26 @@ class ViewController: UIViewController {
     @IBOutlet var txtDate: UITextField!
     @IBAction func abtnCalc(_ sender: UIButton) {
         let ngaythang: String = txtDate.text!
-        
         let kiemtra_ngaythang = Kiem_Tra_NgayThang(ngaythang: ngaythang)
-        if (kiemtra_ngaythang == "OK")
+        if let kiemtra_ngaythang = (kiemtra_ngaythang.0) // kt_ngaythang = kiemtra_ngaythang, tu kiem tra nen dung
         {
-            let arrNgayThang: Array<String> = ngaythang.components(separatedBy: "/")
-            let iNgay:Int = Int(arrNgayThang[0])!
-            let iThang:Int = Int(arrNgayThang[1])!
-            let iNam:Int = Int(arrNgayThang[2])!
-            
-            let thu:String = Ngay_Trong_Tuan(iDay: iNgay, iMonth: iThang, iYear: iNam)
-            lblResult.text = thu
+            let iNgay:Int = Int(kiemtra_ngaythang[0]) // neu dung thi ingay = kt_ngaythang[0]
+            let iThang:Int = Int(kiemtra_ngaythang[1])
+            let iNam:Int = Int(kiemtra_ngaythang[2])
+            let kt = Kiem_Tra_NgayThangNam(iNgay: iNgay, iThang: iThang, iNam: iNam)
+            if kt == "OK"
+            {
+                let thu:String = Ngay_Trong_Tuan(iDay: iNgay, iMonth: iThang, iYear: iNam)
+                lblResult.text = thu
+            }
+            else
+            {
+                lblResult.text = kt
+            }
         }
         else
         {
-            lblResult.text = kiemtra_ngaythang
-
+            lblResult.text = kiemtra_ngaythang.msg
         }
     }
     
@@ -56,14 +60,94 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func Kiem_Tra_NgayThang(ngaythang: String)-> ([Int]?,msg:String) // kiem tra cu phap kieu du lieu cua ngay thang nam
+    {
+        let arrNgayThang: Array<String> = ngaythang.components(separatedBy: "/")
+        if (arrNgayThang.count != 3)
+        {
+            return (nil,"Invalid date") // thoat chuong trinh
+        }
+        else
+        {
+            if let iNgay = Int(arrNgayThang[0]), let iThang = Int(arrNgayThang[1]), let iNam = Int(arrNgayThang[2])
+            {
+               return ([iNgay,iThang,iNam],"")
+            }
+            else
+            {
+                return (nil,"Incorrect date")
+            }
+        }
+    }
+    func Kiem_Tra_NgayThangNam(iNgay: Int,iThang: Int,iNam: Int) -> String// kiem tra ngay thang nam hop le
+    {
+        let arrNormalMonth: Array<Int> = [1,3,5,7,8,10,12]
+        let arrLeapMonth: Array<Int> = [4,6,9,11]
+        if (iThang < 1 || iThang > 12)
+        {
+            return "Nhập sai tháng"
+        }
+        /*
+        if (iNgay < 1 || iNgay > 31)
+        {
+            return "Nhập sai ngày"
+        }
+        */
+        if (iNam < 0)
+        {
+            return "Nhập sai năm"
+        }
+        for i in 0 ... arrNormalMonth.count - 1
+        {
+            if (arrNormalMonth[i] == iThang)
+            {
+                if (iNgay < 1 || iNgay > 31)
+                {
+                    return "Tháng \(iThang) chỉ có từ 1 đến 31 ngày"
+                }
+            }
+        }
+        for j in 0 ... arrLeapMonth.count - 1
+        {
+            if (arrLeapMonth[j] == iThang)
+            {
+                if (iNgay < 1 || iNgay > 30)
+                {
+                    return "Tháng \(iThang) chỉ có từ 1 đến 30 ngày"
+                }
+            }
+        }
+        if (iThang == 2)
+        {
+            if (iNam % 4 == 0)
+            {
+                if (iNgay < 1 || iNgay > 29)
+                {
+                    return "Tháng \(iThang) của năm nhuận \(iNam) chỉ có từ 1 đến 29 ngày"
+                }
+            }
+            else
+            {
+                if (iNgay < 1 || iNgay > 28)
+                {
+                    return "Tháng \(iThang) của năm thường \(iNam) chỉ có từ 1 đến 28 ngày"
+                }
+            }
+        }
+        return "OK"
+    }
+    /*
     func Kiem_Tra_NgayThang(ngaythang: String) -> String{
         let ret = "OK"
         if (ngaythang != "")
         {
             let arrNgayThang: Array<String> = ngaythang.components(separatedBy: "/")
-            let iNgay:Int = Int(arrNgayThang[0])!
-            let iThang:Int = Int(arrNgayThang[1])!
-            let iNam:Int = Int(arrNgayThang[2])! // try catch error
+            let iNgay = Int(arrNgayThang[0])!
+            let iThang = Int(arrNgayThang[1])!
+            let iNam = Int(arrNgayThang[2])! // try catch error
+            if (iNgay != nil || iThang != nil || iNam != nil)
+            {
+                return "Nhập sai kiểu ngày tháng năm. Nhập số theo định dạng dd/mm/yyyy"
             
             if (arrNgayThang[0].characters.count > 2 && arrNgayThang[0].characters.count < 0)
             {
@@ -124,9 +208,10 @@ class ViewController: UIViewController {
                 }
             }
         }
+        }
         return ret
     }
-    
+    */
     func Tong_So_Ngay(iDay:Int,iMonth:Int,iYear:Int)-> Int
     {
         var sumdays: Int=0
